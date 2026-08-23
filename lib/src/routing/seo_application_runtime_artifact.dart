1f44e73f92ca1c6c79c6680503e2f34dd0dee931
Yahya Esen <hello@esen.software>
2026-08-23 22:36:03 +0200

    style: format application runtime artifact

--- lib/src/routing/seo_application_runtime_artifact.dart
+++ lib/src/routing/seo_application_runtime_artifact.dart
@@ -3,11 +3,12 @@ import 'seo_application_runtime.dart';
 /// Collision-free file stem for one build-owned runtime artifact.
 String seoApplicationRuntimeArtifactStem(
   SeoDomFirstApplicationRuntime runtime,
-) => switch (runtime) {
-  SeoDomFirstApplicationRuntimeBundle(:final memberKinds) =>
-    'bundle+${memberKinds.map((kind) => kind.value).join('+')}+${runtime.id}',
-  SeoDomFirstStepperApplicationRuntime()
-      when runtime.id.startsWith('effects-') =>
-    'stepper+${runtime.id}',
-  _ => '${runtime.kind}-${runtime.id}',
-};
+) =>
+    switch (runtime) {
+      SeoDomFirstApplicationRuntimeBundle(:final memberKinds) =>
+        'bundle+${memberKinds.map((kind) => kind.value).join('+')}+${runtime.id}',
+      SeoDomFirstStepperApplicationRuntime()
+          when runtime.id.startsWith('effects-') =>
+        'stepper+${runtime.id}',
+      _ => '${runtime.kind}-${runtime.id}',
+    };
